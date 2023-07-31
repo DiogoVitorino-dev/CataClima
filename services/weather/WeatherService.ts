a3eebdb9d40c9088a5fff12c86cf1283851355c5
Diogo Vitorino Franca <diogovf90@hotmail.com>
2023-07-31 12:46:37 -0300

    added deploy configurations

--- services/weather/WeatherService.ts
+++ services/weather/WeatherService.ts
@@ -1,53 +1,62 @@
 import NetInfo from '@react-native-community/netinfo';
 import Flags from '../../constants/Flags';
-import project from '../../cache/project.json';
+import * as project from '../../cache/project.json'
 import { WeatherProps, CoordinatesProps } from '../../constants/Interfaces';
 import convertCountryCodeToName from '../../utils/convertCountryCodeToName';
 import CountryStateCities from '../../libs/CountryStateCities/CountryStateCities';
 
 // WeatherFetch
 const getWeatherData = async (coords:CoordinatesProps) => {  
-  const {isConnected} = await NetInfo.fetch();  
+  const {isConnected} = await NetInfo.fetch();
   if (isConnected) {
-    const resultsInfo = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${coords.latitude}&lon=${coords.longitude}&appid=${project.WeatherApiKey}&units=metric&lang=pt_br`)    
+    const weatherInfo = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${coords.latitude}&lon=${coords.longitude}&appid=${project.WeatherApiKey}&units=metric&lang=pt_br`)    
 
-    const minmaxTempInfoJSON = await fetch(
+    const minmaxTempInfo = await fetch(
       `https://api.openweathermap.org/data/2.5/forecast?lat=${coords.latitude}&lon=${coords.longitude}&cnt=1&appid=${project.WeatherApiKey}&units=metric`,
     )
 
-    const informationJSON = await resultsInfo.json();
-    const minMaxInfoJSON = await minmaxTempInfoJSON.json();
+    if(weatherInfo.ok && minmaxTempInfo.ok) {
 
-    const state = await CountryStateCities()
-    .findStateCodeOfCity(informationJSON.sys.country,informationJSON.name)
+      const informationJSON = await weatherInfo.json();
+      const minMaxInfoJSON = await minmaxTempInfo.json();
 
-    const country = convertCountryCodeToName(informationJSON.sys.country)
-    
-    const newWeather:WeatherProps = {
-      id:'',
-      city: informationJSON.name,
-      country: country,
-      state: state,
-      feelsLike: parseInt(minMaxInfoJSON.list[0].main.feels_like),
-      maxTemperature: parseInt(minMaxInfoJSON.list[0].main.temp_max),
-      minTemperature: parseInt(minMaxInfoJSON.list[0].main.temp_min),
-      humidity: informationJSON.main.humidity,
-      pressure: informationJSON.main.pressure,
-      pressureUnit:'mBar',
-      temperature: parseInt(informationJSON.main.temp),
-      temperatureUnit:'c',
-      wind: parseFloat((informationJSON.wind.speed * 3.6).toFixed(1)),
-      windUnit:'km/h',
-      weatherDescription: informationJSON.weather[0].description,
-      coords:{
-        latitude:coords.latitude,
-        longitude:coords.longitude
-      },
-      datetime: new Date().toISOString(),
-      weatherMain: informationJSON.weather[0].main.toLowerCase()
-    };     
+      const state = await CountryStateCities()
+      .findStateCodeOfCity(informationJSON.sys.country,informationJSON.name)
 
-    return newWeather;
+      const country = convertCountryCodeToName(informationJSON.sys.country)
+      
+      const newWeather:WeatherProps = {
+        id:'',
+        city: informationJSON.name,
+        country: country,
+        state: state,
+        feelsLike: parseInt(minMaxInfoJSON.list[0].main.feels_like),
+        maxTemperature: parseInt(minMaxInfoJSON.list[0].main.temp_max),
+        minTemperature: parseInt(minMaxInfoJSON.list[0].main.temp_min),
+        humidity: informationJSON.main.humidity,
+        pressure: informationJSON.main.pressure,
+        pressureUnit:'mBar',
+        temperature: parseInt(informationJSON.main.temp),
+        temperatureUnit:'c',
+        wind: parseFloat((informationJSON.wind.speed * 3.6).toFixed(1)),
+        windUnit:'km/h',
+        weatherDescription: informationJSON.weather[0].description,
+        coords:{
+          latitude:coords.latitude,
+          longitude:coords.longitude
+        },
+        datetime: new Date().toISOString(),
+        weatherMain: informationJSON.weather[0].main.toLowerCase()
+      };     
+
+      return newWeather;
+
+    } else
+
+    if (!minmaxTempInfo.ok)    
+      throw new Error(minmaxTempInfo.status + ' ' + minmaxTempInfo.statusText)
+    else
+      throw new Error(weatherInfo.status + ' ' + weatherInfo.statusText)
 
     } else 
       throw new Error(Flags.errors.NOCONNECTION)
